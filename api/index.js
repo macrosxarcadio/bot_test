90ba7d5638c0c6be488665f0dd84e8585056e5b1
German Frate <frate_g@solarsistemas.net>
2023-03-01 15:02:54 -0300

    vercel test

--- api/index.js
+++ api/index.js
@@ -98,6 +98,14 @@ app.listen("/api", () => {
 })
 
 
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  });
+
+
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
